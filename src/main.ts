bd78d8cbe9301372e2104c22bfc9b34e4b798d4c
Ian Cheng <dunz.zheng@gmail.com>
2023-12-20 02:44:10 +0800

    Setting up Vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import './style.css'
 import App from './App.vue'
 import router from "./router.ts";
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App);
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
