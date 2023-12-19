8465b15018743f27cb7828e927deae5e77e04a47
Ian Cheng <dunz.zheng@gmail.com>
2023-12-20 00:37:42 +0800

    Add route page component

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import ProductsList from "./page/products/ProductsList.vue";
+import Register from "./page/auth/Register.vue";
+import Login from "./page/auth/Login.vue";
+import OrdersList from "./page/orders/OrdersList.vue";
+import Checkout from "./page/checkout/Checkout.vue";
+import NotFound from "./page/NotFound.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/products'},
-        {path: '/products', component: null},
+        {path: '/products', component: ProductsList},
         {path: '/auth', component: null, children: [
-            {path: 'register', component: null},
-            {path: 'login', component: null}
+            {path: 'register', component: Register},
+            {path: 'login', component: Login}
         ]},
-        {path: '/orders', component: null},
-        {path: '/checkout', component: null},
-        {path: '/:notFound(.*)', component: null},
+        {path: '/orders', component: OrdersList},
+        {path: '/checkout', component: Checkout},
+        {path: '/:notFound(.*)', component: NotFound},
     ]
 });
 
