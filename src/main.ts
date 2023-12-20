1a1758e7c13ad41664822e2b36f3adf1571308e8
Ian Cheng <dunz.zheng@gmail.com>
2023-12-21 01:01:42 +0800

    Setting up Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import App from './App.vue'
 import router from "./router.ts";
 
 // Vuetify
+import '@fortawesome/fontawesome-free/css/all.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -14,6 +16,13 @@ const app = createApp(App);
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'fa',
+        aliases,
+        sets: {
+            fa,
+        },
+    },
 })
 
 app.use(router);
